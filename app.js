b38c5f70ce443a726af953fe074f5531ed5cfdaa
nmht <thiennmhFX15190@funix.edu.vn>
2022-06-09 13:34:52 +0700

    commit LAB02.4 handle route by 3 middleware

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ const express = require("express");
 const app = express();
 
 // using use function of express as a middleware function
-app.use((req, res, next) => {
-	console.log("Middleware 1 activated!");
+app.use("/", (req, res, next) => {
+	console.log("First always run!");
 	next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+	console.log("Middleware 1 activated!");
+	res.send("<h1>The 'Add Product' Page</h1>");
+});
+
+app.use("/", (req, res, next) => {
 	console.log("Middleware 2 activated!");
 	res.send("<h1>Hello from Express!</h1>");
 });
 
-// create a server that executes specific tasks
-const server = http.createServer(app);
-
-// keep nodejs not shut down server but keep server alive listening
-// listen method takes port to track any incoming request into this port, default port 80
-// listen method takes hostname, default machine name => local machine => localhost
-server.listen(3000);
+// create a server
+app.listen(3000);
