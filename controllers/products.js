8ae2652e0bb867b5b9f33598d33b9b31a418a3c6
nmht <thiennmhFX15190@funix.edu.vn>
2022-06-15 10:48:55 +0700

    commit LAB04.3 MVC add model

--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,22 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
 	res.render("add-product.ejs", {
 		pageTitle: "Add Product",
 		path: "/admin/add-product",
+		activeAddProduct: true,
+		mainCSS: true,
 	});
 };
 
 exports.postAddProduct = (req, res, next) => {
-	products.push({
-		title: req.body.title,
-		activeAddProduct: true,
-		mainCSS: true,
-	});
+	const product = new Product(req.body.title);
+	product.save();
 	res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
+	const products = Product.fetchAll();
 	res.render("shop.ejs", {
 		prods: products,
 		pageTitle: "My Shop",
