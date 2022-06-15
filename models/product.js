45ab2fdf287270d53886555ad3438b1f67f815d5
nmht <thiennmhFX15190@funix.edu.vn>
2022-06-15 14:57:07 +0700

    commit LAB04.17 add delete product feature by postDeleteProduct request

--- models/product.js
+++ models/product.js
@@ -47,6 +47,16 @@ module.exports = class Product {
 		});
 	}
 
+	static deleteById(id) {
+		getProductsFromFile((products) => {
+			const updatedProducts = products.filter((product) => product.id !== id);
+			fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+				if (!err) {
+				}
+			});
+		});
+	}
+
 	static fetchAll(cb) {
 		getProductsFromFile(cb);
 	}
