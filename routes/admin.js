bfb3ffe7b31882b093672c10d6adb7619cf5bd5f
nmht <thiennmhFX15190@funix.edu.vn>
2022-06-15 14:26:09 +0700

    commit LAB04.15 route to edit product page

--- routes/admin.js
+++ routes/admin.js
@@ -17,4 +17,6 @@ router.post("/add-product", adminController.postAddProduct);
 
 router.get("/edit-product/:productId", adminController.getEditProduct);
 
+router.post("/edit-product/:productId", adminController.getEditProduct);
+
 module.exports = router;
