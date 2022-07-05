36a969b92dd3e57d58d5631bc561d18fd5560ed9
nmht <nmhoangthien@gmail.com>
2022-07-06 03:49:49 +0700

    LAB05.22 create and fetch a cart

--- app.js
+++ app.js
@@ -60,8 +60,8 @@ Product.belongsToMany(Cart, { through: CartItem });
 
 // sync DB while starting server
 sequelize
-	.sync({ force: true })
-	// .sync()
+	// .sync({ force: true })
+	.sync()
 	.then((result) => {
 		return User.findByPk(1);
 		// console.log(result);
@@ -75,6 +75,9 @@ sequelize
 	})
 	.then((user) => {
 		// console.log(user);
+		return user.createCart();
+	})
+	.then((cart) => {
 		app.listen(3000);
 	})
 	.catch((err) => {
