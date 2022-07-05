2c5dd1eded16374989935854841dad27efd3042f
nmht <nmhoangthien@gmail.com>
2022-07-05 12:27:29 +0700

    LAB05.3 retrieve data

--- app.js
+++ app.js
@@ -23,7 +23,13 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 // run database
-db.execute("SELECT * FROM products").then().catch();
+db.execute("SELECT * FROM products")
+	.then((result) => {
+		console.log(result[0], result[1]);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 // using use function of express as a middleware function
 app.use(parser.urlencoded({ extended: false }));
