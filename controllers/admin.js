0a9ca0206becf7703b9cff039d6fd3adf255aff0
nmht <nmhoangthien@gmail.com>
2022-07-05 16:23:11 +0700

    LAB05.16 delete product by admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,6 +22,7 @@ exports.postAddProduct = (req, res, next) => {
 		.then((result) => {
 			// console.log(result);
 			console.log("Product Created!");
+			res.redirect("/admin/products");
 		})
 		.catch((err) => console.log(err));
 };
@@ -88,6 +89,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.deleteById(prodId);
-	res.redirect("/admin/products");
+	Product.findByPk(prodId)
+		.then((product) => {
+			return product.destroy();
+		})
+		.then((result) => {
+			console.log("Product Destroyed!");
+			res.redirect("/admin/products");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
