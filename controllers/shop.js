4e4207e4fa6fef7b9a8cd5972f1560b68bbfe895
nmht <nmhoangthien@gmail.com>
2022-07-06 03:58:51 +0700

    LAB05.23 add new products to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -60,32 +60,42 @@ exports.getCart = (req, res, next) => {
 		.catch((err) => {
 			console.log(err);
 		});
-	// Cart.getCart((cart) => {
-	// 	Product.fetchAll((products) => {
-	// 		const cartProducts = [];
-	// 		for (product of products) {
-	// 			const cartProductData = cart.products.find(
-	// 				(prod) => prod.id === product.id
-	// 			);
-	// 			if (cartProductData) {
-	// 				cartProducts.push({ productData: product, qty: cartProductData.qty });
-	// 			}
-	// 		}
-	// 		res.render("shop/cart.ejs", {
-	// 			path: "/cart",
-	// 			pageTitle: "Your Cart",
-	// 			products: cartProducts,
-	// 		});
-	// 	});
-	// });
 };
 
 exports.postCart = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.findById(prodId, (product) => {
-		Cart.addProduct(prodId, product.price);
-	});
-	res.redirect("/cart");
+	let fetchedCart;
+	req.user
+		.getCart()
+		.then((cart) => {
+			fetchedCart = cart;
+			return cart.getProducts({ where: { id: prodId } });
+		})
+		.then((products) => {
+			let product;
+			if (products.length > 0) {
+				product = products[0];
+			}
+			let newQuantity = 1;
+			if (product) {
+				// ..
+			}
+			return Product.findByPk(prodId)
+				.then((product) => {
+					return fetchedCart.addProduct(product, {
+						through: { quantity: newQuantity },
+					});
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		})
+		.then(() => {
+			res.redirect("/cart");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
