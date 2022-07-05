47cf9439e5d20e734137550e6a686769057e61c7
nmht <nmhoangthien@gmail.com>
2022-07-05 13:17:32 +0700

    LAB05.8 install sequelize and connect to DB

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "nmht@2021MMO", {
+	dialect: "mysql",
 	host: "localhost",
-	user: "root",
-	database: "node-complete",
-	password: "nmht@2021MMO",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
