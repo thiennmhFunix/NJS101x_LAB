b5ab7a49a8dc57188bef13cbd4099c3e3197d038
nmht <nmhoangthien@gmail.com>
2022-07-08 00:40:04 +0700

    LAB06.12 delete product

--- models/product.js
+++ models/product.js
@@ -58,6 +58,19 @@ class Product {
 				console.log("~ err", err);
 			});
 	}
+
+	static deleteById(prodId) {
+		const db = getDb();
+		return db
+			.collection("products")
+			.deleteOne({ _id: new mongodb.ObjectId(prodId) })
+			.then((result) => {
+				console.log("Deleted!");
+			})
+			.catch((err) => {
+				console.log("~ err", err);
+			});
+	}
 }
 
 module.exports = Product;
