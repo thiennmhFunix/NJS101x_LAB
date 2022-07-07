4ee79036f4d806b1bf93a3aa2a7247767a0496d2
nmht <nmhoangthien@gmail.com>
2022-07-08 00:34:56 +0700

    LAB06.11 move format objectId task from controller to model product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,4 @@
-const mongodb = require("mongodb");
 const Product = require("../models/product");
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
 	res.render("admin/edit-product.ejs", {
@@ -63,7 +60,7 @@ exports.postEditProduct = (req, res, next) => {
 		updatedPrice,
 		updatedDescription,
 		updatedImageUrl,
-		new ObjectId(prodId)
+		prodId
 	);
 	product
 		.save()
