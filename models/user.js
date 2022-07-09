24f26b59cd54b43324f429c5c71a9cddb0e1d37d
nmht <nmhoangthien@gmail.com>
2022-07-09 09:32:30 +0700

    LAB06.23 add relational order data of user and product item

--- models/user.js
+++ models/user.js
@@ -80,9 +80,17 @@ class User {
 
 	addOrder() {
 		const db = getDb();
-		return db
-			.collection("orders")
-			.insertOne(this.cart)
+		return this.getCart()
+			.then((products) => {
+				const order = {
+					items: products,
+					user: {
+						_id: new ObjectId(this._id),
+						name: this.name,
+					},
+				};
+				return db.collection("orders").insertOne(order);
+			})
 			.then((result) => {
 				this.cart = { items: [] };
 				return db
@@ -93,6 +101,22 @@ class User {
 					);
 			});
 	}
+
+	// getOrders() {
+	// 	const db = getDb();
+	// 	return db
+	// 		.collection("orders")
+	// 		.insertOne(this.cart)
+	// 		.then((result) => {
+	// 			this.cart = { items: [] };
+	// 			return db
+	// 				.collection("users")
+	// 				.updateOne(
+	// 					{ _id: new ObjectId(this._id) },
+	// 					{ $set: { cart: { items: [] } } }
+	// 				);
+	// 		});
+	// }
 
 	static findByPk(userId) {
 		const db = getDb();
