530ae1f3b12800f18ef8e444aa9d98de296ee913
nmht <nmhoangthien@gmail.com>
2022-07-17 02:14:10 +0700

    LAB07.1 connect mongoDB server with mongoose

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const rootDir = require("./util/path");
 
 const express = require("express");
 const parser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-
-const mongoConnect = require("./util/database").mongoConnect;
 
 const User = require("./models/user");
 
@@ -30,22 +29,27 @@ app.use(parser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(rootDir, "public")));
 
-app.use((req, res, next) => {
-	User.findByPk("62c71c8ed6a839870aef4b09")
-		.then((user) => {
-			req.user = new User(user.name, user.email, user.cart, user._id);
-			next();
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-});
+// app.use((req, res, next) => {
+// 	User.findByPk("62c71c8ed6a839870aef4b09")
+// 		.then((user) => {
+// 			req.user = new User(user.name, user.email, user.cart, user._id);
+// 			next();
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 		});
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-	app.listen(3000);
-});
+mongoose
+	.connect(
+		"mongodb+srv://nmht:nmht2021@mongodb-test.p8duu.mongodb.net/?retryWrites=true&w=majority"
+	)
+	.then((result) => {
+		app.listen(3000);
+	})
+	.catch((err) => console.log(err));
