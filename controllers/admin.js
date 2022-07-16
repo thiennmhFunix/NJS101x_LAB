bac9540d93a7fd8e713bc7e93adb877caa55058d
nmht <nmhoangthien@gmail.com>
2022-07-17 02:54:26 +0700

    LAB07.7 delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -93,7 +93,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.deleteById(prodId)
+	Product.findByIdAndRemove(prodId)
 		.then((result) => {
 			console.log("Product Destroyed!");
 			res.redirect("/admin/products");
