788a2f62d110da14c35a9cde9ef26364ada1af29
nmht <nmhoangthien@gmail.com>
2022-07-19 14:38:04 +0700

    LAB08.12 implement authentication flow

--- app.js
+++ app.js
@@ -69,18 +69,6 @@ app.use(errorController.get404);
 mongoose
 	.connect(MONGODB_URI)
 	.then((result) => {
-		User.findOne().then((user) => {
-			if (!user) {
-				const user = new User({
-					name: "Thien",
-					email: "thien@test.com",
-					cart: {
-						items: [],
-					},
-				});
-				user.save();
-			}
-		});
 		app.listen(3000);
 	})
 	.catch((err) => console.log(err));
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,18 @@
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-	const isLoggedIn = req.get("Cookie").split("=")[1];
 	res.render("auth/login.ejs", {
 		path: "/login",
 		pageTitle: "Login",
-		isAuthenticated: isLoggedIn,
+		isAuthenticated: false,
+	});
+};
+
+exports.getSignup = (req, res, next) => {
+	res.render("auth/signup.ejs", {
+		path: "/signup",
+		pageTitle: "Signup",
+		isAuthenticated: false,
 	});
 };
 
@@ -20,6 +27,32 @@ exports.postLogin = (req, res, next) => {
 			});
 		})
 		.catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	const confirmPassword = req.body.confirmPassword;
+	// ignore validate atm
+	// check if email has already signup
+	User.findOne({ email: email })
+		.then((userDoc) => {
+			if (userDoc) {
+				return res.redirect("/signup");
+			}
+			const user = new User({
+				email: email,
+				password: password,
+				cart: { items: [] },
+			});
+			return user.save();
+		})
+		.then((result) => {
+			res.redirect("/login");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.postLogout = (req, res, next) => {
