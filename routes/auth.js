80ba539c8918ac080d39437fdeaf47925c960765
nmht <nmhoangthien@gmail.com>
2022-07-20 13:57:50 +0700

    LAB09.4 more validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator/check");
+const { check, body } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
 
@@ -11,18 +11,28 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
+// [] is not a must, used for better tracking code
+// validator will process in order from head to tail as listed in router post
 router.post(
 	"/signup",
-	check("email")
-		.isEmail()
-		.withMessage("Please enter a valid email.")
-		// custom validator
-		.custom((value, { req }) => {
-			if (value === "name0@mail.com") {
-				throw new Error("This email address is not allowed.");
-			}
-			return true;
-		}),
+	[
+		check("email")
+			.isEmail()
+			.withMessage("Please enter a valid email.")
+			// custom validator
+			.custom((value, { req }) => {
+				if (value === "name0@mail.com") {
+					throw new Error("This email address is not allowed.");
+				}
+				return true;
+			}),
+		body(
+			"password",
+			"Please enter a password with only numbers and text and at least 5 characters."
+		)
+			.isLength({ min: 5 })
+			.isAlphanumeric(),
+	],
 	authController.postSignup
 );
 
