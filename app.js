b2d305394ecd2954dc463665a2111747e4d2a8f7
nmht <nmhoangthien@gmail.com>
2022-09-08 15:52:13 +0700

    LAB10.5 filter files by mimetype

--- app.js
+++ app.js
@@ -34,6 +34,18 @@ const fileStorage = multer.diskStorage({
 	},
 }); // diskStorage is a storage engine
 
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === "image/png" ||
+		file.mimetype === "image/jpg" ||
+		file.mimetype === "image/jpeg"
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
 app.set("view-engine", "ejs");
 app.set("views", "views");
 
@@ -42,7 +54,9 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single("image")); // multer to handle multipart form data
+app.use(
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+); // multer to handle multipart form data
 app.use(express.static(path.join(rootDir, "public")));
 app.use(
 	session({
